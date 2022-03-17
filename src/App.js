8d326c93274d1d99d9089c5103036457d9e5eaf9
oliaxe <olivia.axellie@gmail.com>
2022-03-17 14:09:01 +0100

    firstpush

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Card/>
         
       </header>
     </div>
---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+export default function Card(){
+    return  (
+        <div className="container my-5 mx-auto">
+
+            <div className="card">
+            <img src="https://via.placeholder.com/150" className="time card-image-top" alt="korv"/>
+            <div className="icon bg-light mx-auto text-center">
+            </div>
+
+                <h5 className="card--text">CITY NAME</h5>
+                <div className="card--text">weather condition</div>
+                <div className="display-4 my-4">
+                <span>temp</span>    
+                <span>&deg;C</span>
+            </div>
+            </div>
+
+            </div>
+
+        )
+}

4e4271942b0b67c2bc7f2bf3cb177182aff7b524
Levent Yildirim <leventyildirim@icloud.com>
2022-03-22 17:12:55 +0100

    Completed LastSearched component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,10 @@
-import LastSearched from "./utils/LastSearched/LastSearched.js";
-import React, { useEffect, useState } from "react";
+import LastSearched from "./components/LastSearched/LastSearched.js";
 
 function App() {
-  const [cities, setCities] = useState([]);
-
-  useEffect(() => {
-    getCities();
-  }, []);
-
-  function getCities() {
-    let temp = [];
-    for (let i = 0; i < localStorage.length; i++) {
-      let cityObj = localStorage.getItem(localStorage.key(i));
-      temp.push(cityObj);
-    }
-
-    setCities(temp);
-  }
-
-  function addCity() {
-    //get city input value
-    const city = document.getElementById("cityText").value.trim();
-
-    if (
-      localStorage.getItem(city) == null ||
-      localStorage.getItem(city) == ""
-    ) {
-      localStorage.setItem(city, city);
-    }
-
-    getCities();
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <input id="cityText"></input>
-        <button id="btn" onClick={addCity}>
-          Click here
-        </button>
-
-        <LastSearched cities={cities} />
+        <LastSearched/>
       </header>
     </div>
   );

84aa634fed07bcc9a2fabb40cae840a9ba55cb2f
oliaxe <olivia.axellie@gmail.com>
2022-03-23 17:08:29 +0100

    big cities

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Card from './components/Card/Card'
+import Card from "./components/Card/Card";
+import BigCities from "./components/BigCities/BigCities";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card/>
-        
-      </header>
+        <Card />
+        </header>
+        <div className="cities">
+        <BigCities city="Longyearbyen" />
+        <BigCities city="London" />
+        <BigCities city="Stockholm" />
+        <BigCities city="Mumbai" />
+        <BigCities city="Hanoi" />
+        <BigCities city="Rome" />
+        <BigCities city="Berlin" />
+    </div>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,12 @@
+import './Card.css'
+
 export default function Card(){
     return  (
-        <div className="container my-5 mx-auto">
-
+        <div className="container">
             <div className="card">
             <img src="https://via.placeholder.com/150" className="time card-image-top" alt="korv"/>
             <div className="icon bg-light mx-auto text-center">
             </div>
-
                 <h5 className="card--text">CITY NAME</h5>
                 <div className="card--text">weather condition</div>
                 <div className="display-4 my-4">

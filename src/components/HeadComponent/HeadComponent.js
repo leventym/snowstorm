58b2bce6f18f4a53b588beee8a0ca180aaa09a49
Levent Yildirim <leventyildirim@icloud.com>
2022-03-24 14:23:20 +0100

    added removeItem to SearchBar.js

--- src/components/HeadComponent/HeadComponent.js
+++ src/components/HeadComponent/HeadComponent.js
@@ -5,7 +5,7 @@ import Geolocation from "../Geolocation/Geolocation";
 
 function HeadComponent() {
 
-    const apiKey = 'IhuWC2KGme0zeezZJuBpgOh3JrDNxL26'
+    const apiKey = 'asYR1vouPBrsWDLvHuxRw55envEYujJ5'
     
       return (
         <div className="App">

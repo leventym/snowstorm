7826be6d3a648eede5e3bcbaf7954aa785476d34
anna <Anna.Wallin@yh.nackademin.se>
2022-03-24 10:20:11 +0100

    updated geolocation

---
+++ src/components/Geolocation/Geolocation.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from "react";
+import './Geolocation.css';
+
+export default function Geolocation() {    
+
+    const key = 'GAYJdZs9O3oiKGC0GGeQYkyQE83C06AE';
+    // const key = 'M2CnSr9c7j4vgM4jJBRSxt2jsAekQvbS'; //OLIVIASNOM
+    const [pos, setPos] = useState([null, null])
+    const [localWeather, setLocalWeather] = useState({});  
+    const [location, setLocation] = useState('');    
+    let locationKey = '';  
+    
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) =>{
+            setPos([position.coords.latitude, position.coords.longitude])
+        })
+    })
+    const getLocation = async () => {  
+            const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${key}&q=${pos[0]}%2C${pos[1]}`);
+            const data = await response.json();
+            console.log(pos[0], pos[1]);
+            setLocation(data.ParentCity.LocalizedName);
+            locationKey = data.Key;
+            console.log(locationKey);
+            return locationKey;
+        };
+    
+    const getWeather = async () => {
+        const response = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${key}`);
+        const data = await response.json();
+        setLocalWeather(data[0]);
+    }
+
+    const getLocalWeather = async () => {
+        await getLocation();
+        await getWeather();
+        console.log(localWeather);
+    }
+        
+    return (
+        
+        <div className="geo">
+            <div className="geo--card">
+                <button className="geo--button" onClick={getLocalWeather}>Där du är</button>
+                
+                <h5>{location}</h5>
+                {localWeather.WeatherText ? <h5>{localWeather.WeatherText}</h5> : null}
+                {localWeather.WeatherText ? <p>{localWeather.Temperature.Metric.Value} C</p> : null}
+
+            </div>
+        </div>
+        )
+    }
